3e5d894e3a1dedee0054df5f7867a6b5efc2b0fc
hzozimo <hzozimo@gmail.com>
2021-05-11 00:48:33 -0300

    Updates

---
+++ precache-manifest.2b2ef90ed6d1f8ddf0a09a4b66571ec8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "683962780f7cb4f90e4203df771d943a",
+    "url": "/exercise-pokedex-router/index.html"
+  },
+  {
+    "revision": "ff02f5151346fbc3319b",
+    "url": "/exercise-pokedex-router/static/css/main.118954d3.chunk.css"
+  },
+  {
+    "revision": "a3857df003da032f6c81",
+    "url": "/exercise-pokedex-router/static/js/2.393040c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/exercise-pokedex-router/static/js/2.393040c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff02f5151346fbc3319b",
+    "url": "/exercise-pokedex-router/static/js/main.19734ac0.chunk.js"
+  },
+  {
+    "revision": "873c55af0c41c2f98a19",
+    "url": "/exercise-pokedex-router/static/js/runtime-main.9b459d4c.js"
+  }
+]);
